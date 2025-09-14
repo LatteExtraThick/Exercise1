53a9fc9790256a07161230a703c657d495c3c724
LatteExtraThick <sidpro2010@gmail.com>
2025-09-14 22:25:05 +0530

    Add files via upload

---
+++ PPS Exercise1.1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    printf("###########################################################\n");
+    printf("                             Welcome!\n\n");
+    printf("Please choose a number from the following options:\n");
+    printf("1.Play the game!\n");
+    printf("2.Demo the game!\n");
+    printf("3.Exit\n\n");
+    printf("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n");
+    return 0;
+}
